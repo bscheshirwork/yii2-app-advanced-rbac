f348c2fd2fbc36e1e803d2f92a7c725ed3095fd9
bscheshirwork <bscheshir.work@gmail.com>
2017-04-14 12:04:41 +0300

    :gear: env for dev

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=db;dbname=yii2advanced',
+            'username' => 'yii2advanced',
+            'password' => 'yii2advanced',
             'charset' => 'utf8',
         ],
         'mailer' => [
