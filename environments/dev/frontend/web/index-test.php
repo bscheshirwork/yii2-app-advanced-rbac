1dde3b1aa03b15b87676fe8fbdf4f765d621136e
bscheshirwork <bscheshir.work@gmail.com>
2017-09-06 14:05:36 +0300

    selenium access to tests

--- environments/dev/frontend/web/index-test.php
+++ environments/dev/frontend/web/index-test.php
@@ -1,7 +1,13 @@
 <?php
 
 // NOTE: Make sure this file is not accessible when deployed to production
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+//if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+//    die('You are not allowed to access this file.');
+//}
+
+//NOTE: DOUBLE CHECK! Make sure this file is not accessible when deployed to production
+//check if not in same subnet /16 (255.255.0.0). Use for docker Codeception tests
+if ((ip2long(@$_SERVER['REMOTE_ADDR']) ^ ip2long(@$_SERVER['SERVER_ADDR'])) >= 2 ** 16) {
     die('You are not allowed to access this file.');
 }
 
