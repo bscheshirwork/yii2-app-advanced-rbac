aa6707a5d80f02e47811002ed58e4274f84f4353
bscheshirwork <bscheshir.work@gmail.com>
2017-11-10 14:43:34 +0300

    add test for fail send email

---
+++ frontend/tests/functional/ContactFailCest.php
@@ -0,0 +1,30 @@
+<?php
+namespace frontend\tests\functional;
+
+use frontend\tests\FunctionalTester;
+use Yii;
+
+/* @var $scenario \Codeception\Scenario */
+
+class ContactFailCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        \Yii::$app->set('mailer', \frontend\fixtures\MockMailer::class);
+        $I->configureMailer();
+        $I->amOnPage(['site/contact']);
+    }
+
+    public function checkContactSubmitCorrectDataSendFail(FunctionalTester $I)
+    {
+        $I->submitForm('#contact-form', [
+            'ContactForm[name]' => 'tester',
+            'ContactForm[email]' => 'tester@example.com',
+            'ContactForm[subject]' => 'test subject',
+            'ContactForm[body]' => 'test content',
+            'ContactForm[verifyCode]' => 'testme',
+        ]);
+        $I->dontSeeEmailIsSent();
+        $I->see(Yii::t('main', 'There was an error sending your message.'));
+    }
+}
