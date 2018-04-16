93779d4250529c5b4cf8136a64ae61b17b78d3ee
bscheshirwork <bscheshir.work@gmail.com>
2018-04-16 17:04:50 +0300

    codeception dependency refresh

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -6,4 +6,6 @@ Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 
 //extend components
 Yii::setAlias('components', '@common/components');
+
 //temporary redefine framework components: change path to file.php; not to Class
+Yii::$classMap['yii\caching\Cache'] = '@common/components/redefine/caching/Cache.php';
